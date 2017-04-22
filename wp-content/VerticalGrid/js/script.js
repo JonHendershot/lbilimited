f9f1618eac5864c7bec6bbd1afa76f725552f724
JonHendershot <jon@staygrind.com>
2017-04-21 20:11:46 -0400

    update vertical grid test

--- wp-content/VerticalGrid/js/script.js
+++ wp-content/VerticalGrid/js/script.js
@@ -1,3 +1,14 @@
-$(window).load(function(){
-	console.log('fuckyea!');
-});
+(function verticalGrid($){
+	$('img').each(function(){
+		var image = $(this),
+			wrapper = image.parent(),
+			imgW = image.width(),
+			imgH = image.height(),
+			wrapperW = wrapper.width(),
+			wrapperH = wrapper.height();
+			
+		if(imgH < wrapperH){
+			image.css({'height':'100%','width':'auto'});
+		}	
+	});
+}(jQuery));
