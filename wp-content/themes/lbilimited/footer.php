9ba861abe8cc870372717ed2da4de73e979002ba
JonHendershot <jon@staygrind.com>
2017-04-30 16:44:03 -0400

    update theme options, footer.php content & styles

--- wp-content/themes/lbilimited/footer.php
+++ wp-content/themes/lbilimited/footer.php
@@ -5,18 +5,62 @@
  * Contains the closing of the #content div and all content after.
  */
 
+// Call global options data and establish variables for the template -- this pulls information from the theme options found in the appearance menu, as established in theme_options.php
+$options = get_option('lbilimited_options');
+
+	// CTA Variables
+	$CTA_title = $options['LBI_f_CTA'];
+	$CTA_sub = $options['LBI_f_CTA_sub'];
+	$CTA_mailer = $options['LBI_f_mailer_shortcode'];
+	$CTA_bg_image = $options['footer_background'];
+	
+	// Meta variables
+	$copyright = $options['LBI_f_copyright'];
+	$email = $options['LBI_f_email'];
+	
+	// Social Links
+	$facebook_link = $options['LBI_facebook'];
+	$twitter_link = $options['LBI_twitter'];
+	$instagram_link = $options['LBI_instagram'];
+	$youtube_link = $options['LBI_youtube'];
 ?>
 
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="newsletter">
-			<h3>Let's stay in touch</h3>
-			<p>Current Offerings Updates and Classic Car News</p>
-			<!-- subscribe goes here -->
+		<div class="newsletter-cta" style="background-image: url(<?php echo $CTA_bg_image; ?>);">
+			<?php
+				echo "<h3>$CTA_title</h3>
+				<p>$CTA_sub</p>" .
+				do_shortcode($CTA_mailer);
+			?>
 		</div>
 		<div class="footer-meta">
-			
+			<div class="container">
+				<div class="copyright"><?php echo $copyright; ?></div>
+				<div class="email"><?php echo "<a href='mailto:$email'>$email</a>"; ?></div>
+				<div class="socials">
+					<?php
+					// only apply sections for the social links if they're saved in the options page
+					
+					if($facebook_link){
+						echo "<a href='$facebook_link' class='social-link facebook'><i class='fa fa-facebook' aria-hidden='true'></i></a>";
+					}
+					if($instagram_link){
+						echo "<a href='$instagram_link' class='social-link instagram'><i class='fa fa-instagram' aria-hidden='true'></i></a>";
+					}
+					if($youtube_link){
+						echo "<a href='$youtube_link' class='social-link youtube'><i class='fa fa-youtube' aria-hidden='true'></i></a>";
+					}
+					if($twitter_link){
+						echo "<a href='$twitter_link' class='social-link twitter'><i class='fa fa-twitter' aria-hidden='true'></i></a>";
+					}	
+						
+					
+					
+					?>
+				</div>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/lbilimited/theme_options.php
+++ wp-content/themes/lbilimited/theme_options.php
@@ -5,7 +5,7 @@
  */
 function lbilmited_menu()
 {
-  add_theme_page( 'Theme Option', 'LBI Options', 'manage_options', 'lbilmited_options.php', 'lbilmited_page');  
+  add_theme_page( 'Theme Option', 'LBI Options', 'manage_options', 'lbilimited_options.php', 'lbilmited_page');  
 }
 add_action('admin_menu', 'lbilmited_menu');
 
@@ -17,12 +17,13 @@ function lbilmited_page()
 {
 ?>
     <div class="section panel">
-      <h1>Custom Theme Options</h1>
+      <h1>LBI Limited - Theme Options</h1>
+      <hr>
       <form method="post" enctype="multipart/form-data" action="options.php" id="admin-options">
         <?php 
-          settings_fields('lbilmited_options'); 
+          settings_fields('lbilimited_options'); 
         
-          do_settings_sections('lbilmited_options.php');
+          do_settings_sections('lbilimited_options.php');
         ?>
             <p class="submit">  
                 <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />  
@@ -45,69 +46,20 @@ add_action( 'admin_init', 'LBI_register_settings' );
 function LBI_register_settings()
 {
     // Register the settings with Validation callback
-    register_setting( 'lbilmited_options', 'lbilmited_options', 'LBI_validate_settings' );
+    register_setting( 'lbilimited_options', 'lbilimited_options', 'LBI_validate_settings' );
 
     // Add settings section
-    add_settings_section( 'LBI_contact_section', 'Contact Information', 'LBI_display_section', 'lbilmited_options.php' );
-    add_settings_section( 'LBI_text_section', 'Social Links', 'LBI_display_section', 'lbilmited_options.php' );
-
+    add_settings_section( 'LBI_contact_section', 'Contact Information', 'LBI_display_section', 'lbilimited_options.php' );
+    add_settings_section( 'LBI_social_section', 'Social Links', 'LBI_display_section', 'lbilimited_options.php' );
+    add_settings_section( 'LBI_footer_section', 'Site Footer Options', 'LBI_display_section', 'lbilimited_options.php' );
     
     
-    // Create About Title field for Footer
-    $field_args = array(
-      'type'      => 'text',
-      'id'        => 'LBI_fabout_title',
-      'name'      => 'LBI_fabout_title',
-      'desc'      => 'Footer About Title',
-      'std'       => '',
-      'label_for' => 'LBI_fabout_title',
-      'class'     => 'css_class'
-    );
-
-    add_settings_field( 'LBI_fabout_title', 'Footer About Title', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_contact_section', $field_args );
-    
-     // Create About field for footer
-    $field_args = array(
-      'type'      => 'text',
-      'id'        => 'LBI_fabout_text',
-      'name'      => 'LBI_fabout_text',
-      'desc'      => 'About paragraph for footer',
-      'std'       => '',
-      'label_for' => 'LBI_fabout_text',
-      'class'     => 'css_class'
-    );
-
-    add_settings_field( 'LBI_fabout_text', 'About Text', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_contact_section', $field_args );
-    
-    // Create field for Know Your World Purchase Link
-    $field_args = array(
-      'type'      => 'text',
-      'id'        => 'kyw_purchase',
-      'name'      => 'kyw_purchase',
-      'desc'      => 'Link for button at bottom of Know Your World posts',
-      'std'       => '',
-      'label_for' => 'kyw_purchase',
-      'class'     => 'css_class'
-    );
-
-    add_settings_field( 'kyw_purchase', 'Know Your World Link', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_contact_section', $field_args );
-    
-    // Create field for Know Your World Purchase text
-    $field_args = array(
-      'type'      => 'text',
-      'id'        => 'kyw_purchase_text',
-      'name'      => 'kyw_purchase_text',
-      'desc'      => 'Text for Know Your World Purchase button',
-      'std'       => '',
-      'label_for' => 'kyw_purchase_text',
-      'class'     => 'css_class'
-    );
-
-    add_settings_field( 'kyw_purchase_text', 'Know Your World Purchase Text', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_contact_section', $field_args );
-
+    ////////////////////////////////////
+    // - - - - SOCIAL OPTIONS - - - - //
+    ////////////////////////////////////    
     
     
-    // Create textbox field
+    // Facebook
     $field_args = array(
       'type'      => 'text',
       'id'        => 'LBI_facebook',
@@ -118,9 +70,9 @@ function LBI_register_settings()
       'class'     => 'css_class'
     );
 
-    add_settings_field( 'facebook_link', 'Facebook Link', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_text_section', $field_args );
+    add_settings_field( 'LBI_facebook', 'Facebook Link', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_social_section', $field_args );
     
-    // Create textbox field
+    // Instagram
     $field_args = array(
       'type'      => 'text',
       'id'        => 'LBI_instagram',
@@ -131,21 +83,38 @@ function LBI_register_settings()
       'class'     => 'css_class'
     );
 
-    add_settings_field( 'insta_link', 'Instagram Link', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_text_section', $field_args );
+    add_settings_field( 'LBI_instagram', 'Instagram Link', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_social_section', $field_args );
     
     
-    // Create textbox field
+    // Twitter
     $field_args = array(
       'type'      => 'text',
       'id'        => 'LBI_twitter',
       'name'      => 'LBI_twitter',
-      'desc'      => 'twitter Social Link',
+      'desc'      => 'Twitter Social Link',
       'std'       => '',
       'label_for' => 'LBI_twitter',
       'class'     => 'css_class'
     );
 
-    add_settings_field( 'twitter_link', 'twitter Link', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_text_section', $field_args );
+    add_settings_field( 'LBI_twitter', 'Twitter Link', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_social_section', $field_args );
+    
+    // YouTube
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'LBI_youtube',
+      'name'      => 'LBI_youtube',
+      'desc'      => 'Youtube Social Link',
+      'std'       => '',
+      'label_for' => 'LBI_youtube',
+      'class'     => 'css_class'
+    );
+
+    add_settings_field( 'LBI_youtube', 'YouTube Link', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_social_section', $field_args );
+    
+    
+    
+    
     
     
 	// Search Header Image Upload
@@ -159,7 +128,7 @@ function LBI_register_settings()
       'class'     => ' left-float header_image LBI_logo'
     );
 
-    add_settings_field( 'LBI_logo', 'Header Logo', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_text_section', $field_args );
+    add_settings_field( 'LBI_logo', 'Header Logo', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_social_section', $field_args );
     
     // Alternate Header Logo Color
     $field_args = array(
@@ -172,35 +141,93 @@ function LBI_register_settings()
       'class'     => ' left-float header_image gfa-alt'
     );
 
-    add_settings_field( 'LBI_logo_alt1', 'Header Logo - Alternate 1', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_text_section', $field_args );
+    add_settings_field( 'LBI_logo_alt1', 'Header Logo - Alternate 1', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_social_section', $field_args );
     
-        
-    // Know Your World Header Logo
+    
+    ////////////////////////////////////
+    // - - - - FOOTER OPTIONS - - - - //
+    ////////////////////////////////////
+    
+    // Footer CTA
     $field_args = array(
       'type'      => 'text',
-      'id'        => 'know-your-world-logo',
-      'name'      => 'know-your-world-logo',
-      'desc'      => '<input data-category="kyw-logo" class="left-float image-trigger" type="button" value="Upload Image"><p>Know Your World Archive Page Logo</p>',
+      'id'        => 'LBI_f_CTA',
+      'name'      => 'LBI_f_CTA',
+      'desc'      => 'Heading to initiate the call to action to signing up for newsletter',
       'std'       => '',
-      'label_for' => 'know-your-world-logo',
-      'class'     => ' left-float header_image kyw-logo'
-    );
-
-    add_settings_field( 'know-your-world-logo', 'Know Your World Header Logo', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_text_section', $field_args );
-    
-
-// Footer Copyright
-    $field_args = array(
-      'type'      => 'text',
-      'id'        => 'LBI_footer_copy',
-      'name'      => 'LBI_footer_copy',
-      'desc'      => 'Text for footer copyright',
-      'std'       => '',
-      'label_for' => 'LBI_footer_copy',
+      'label_for' => 'LBI_f_CTA',
       'class'     => 'css_class'
     );
 
-    add_settings_field( 'LBI_footer_copy', 'Footer Copyright', 'LBI_display_setting', 'lbilmited_options.php', 'LBI_contact_section', $field_args );    
+    add_settings_field( 'LBI_f_CTA', 'Newsletter CTA Heading', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_footer_section', $field_args );
+
+	// Footer CTA Subtitle
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'LBI_f_CTA_sub',
+      'name'      => 'LBI_f_CTA_sub',
+      'desc'      => 'Subtitle text for the newsletter CTA',
+      'std'       => '',
+      'label_for' => 'LBI_f_CTA_sub',
+      'class'     => 'css_class'
+    );
+
+    add_settings_field( 'LBI_f_CTA_sub', 'Newsletter CTA Subtitle', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_footer_section', $field_args );   
+       
+    // Footer CTA Mailing-list Shortcode to capture subscriptions
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'LBI_f_mailer_shortcode',
+      'name'      => 'LBI_f_mailer_shortcode',
+      'desc'      => 'Shortcode for mailing-list subscription form',
+      'std'       => '',
+      'label_for' => 'LBI_f_mailer_shortcode',
+      'class'     => 'css_class'
+    );
+
+    add_settings_field( 'LBI_f_mailer_shortcode', 'Mailer Shortcode', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_footer_section', $field_args );
+        
+    // Footer CTA Background
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'footer_background',
+      'name'      => 'footer_background',
+      'desc'      => '<input data-category="footer-bg" class="left-float image-trigger" type="button" value="Upload Image"><p>Image to Display behind newsletter CTA</p>',
+      'std'       => '',
+      'label_for' => 'footer_background',
+      'class'     => ' left-float header_image footer-bg'
+    );
+
+    add_settings_field( 'footer_background', 'Footer Background', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_footer_section', $field_args );
+    
+    // Footer Copyright
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'LBI_f_copyright',
+      'name'      => 'LBI_f_copyright',
+      'desc'      => 'Copyright text for site footer',
+      'std'       => '',
+      'label_for' => 'LBI_f_copyright',
+      'class'     => 'css_class'
+    );
+
+    add_settings_field( 'LBI_f_copyright', 'Footer Copyright', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_footer_section', $field_args );
+    
+
+    // Footer Email
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'LBI_f_email',
+      'name'      => 'LBI_f_email',
+      'desc'      => 'Email address to be displayed in site footer',
+      'std'       => '',
+      'label_for' => 'LBI_f_email',
+      'class'     => 'css_class'
+    );
+
+    add_settings_field( 'LBI_f_email', 'Footer Email Address', 'LBI_display_setting', 'lbilimited_options.php', 'LBI_footer_section', $field_args );
+
+    
   
 }
 
@@ -234,7 +261,7 @@ function LBI_display_setting($args)
 {
     extract( $args );
 
-    $option_name = 'lbilmited_options';
+    $option_name = 'lbilimited_options';
 
     $options = get_option( $option_name );
 
