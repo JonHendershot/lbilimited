fa11e57ebd63d71222068b020dbe046ee30278ac
Jonathan Hendershot <jonhendershot@MacBook-Pro.fios-router.home>
2017-06-06 16:56:19 -0400

    remove static action link from searchform

--- wp-content/themes/lbilimited/searchform.php
+++ wp-content/themes/lbilimited/searchform.php
@@ -4,7 +4,7 @@
 	// This search form is displayed when a user clicks the 'click to search' button in the nav-bar
 	// ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
 ?>
-<form role="search" method="get" class="search-form lightbox_content" action="http://localhost:8888/lbi/">
+<form role="search" method="get" class="search-form lightbox_content" action="<?php echo home_url(); ?>">
 	<label>
 		<div class="search-title">I would like to find:</div>
 		<input type="text" id="focus_field" class="search-field" placeholder="" value="" name="s" autocomplete="off">
