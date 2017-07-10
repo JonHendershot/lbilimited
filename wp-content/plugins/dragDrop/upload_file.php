59241358ca4e6fc375d13cd8a047abf2bdb87b6a
Jon Hendershot <jon@wearethelife.com>
2017-07-10 14:48:38 -0400

    test hardcode link to wp-load

--- wp-content/plugins/dragDrop/upload_file.php
+++ wp-content/plugins/dragDrop/upload_file.php
@@ -17,7 +17,7 @@
 */
 
 	$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
-	require_once( $parse_uri[0] . 'wp-load.php' );
+	require_once( 'http://132.148.86.26/~lbi/wp-load.php' );
 	
 	
 	// Variables
